799efe4ff2742afe79d8d8937dff0962884b2541
toshinori.sato <toshinori.sato@goodpatch-s.com>
2023-01-24 16:37:52 +0900

    undo redo sample
    - keymap plugin
    - history plugin

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import {schema} from "prosemirror-schema-basic";
 import {EditorState} from "prosemirror-state";
 import {EditorView} from "prosemirror-view";
+import {undo, redo, history} from "prosemirror-history"
+import {keymap} from "prosemirror-keymap"
 
 let state: EditorState;
 let view: EditorView;
 
 document.addEventListener('DOMContentLoaded', () => {
-  state = EditorState.create({schema});
+  state = EditorState.create({
+    schema,
+    plugins: [
+      history(),
+      keymap({"Mod-z": undo, "Mod-shift-z": redo}),
+    ],
+  });
+
   view = new EditorView(document.body, {
     state,
     dispatchTransaction(tr) {
