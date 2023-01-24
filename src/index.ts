02cd5c62094032d8dad99c093b4bf461eac7e8b3
toshinori.sato <toshinori.sato@goodpatch-s.com>
2023-01-24 16:34:07 +0900

    transaction sample

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import {schema} from "prosemirror-schema-basic";
 import {EditorState} from "prosemirror-state";
 import {EditorView} from "prosemirror-view";
 
+let state: EditorState;
+let view: EditorView;
+
 document.addEventListener('DOMContentLoaded', () => {
-  let state = EditorState.create({schema});
-  let view = new EditorView(document.body, {state});
+  state = EditorState.create({schema});
+  view = new EditorView(document.body, {
+    state,
+    dispatchTransaction(tr) {
+      console.log(
+        "Document size went from", tr.before.content.size,
+        "to", tr.doc.content.size,
+      )
+      let newState = view.state.apply(tr);
+      view.updateState(newState);
+    },
+  });
 });
